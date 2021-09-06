f11d5cd7806453d53e50e74ab00100e77a3e23fc
José Expósito <jose.exposito89@gmail.com>
2021-09-06 07:36:17 +0200

    Add support for GNOME 41

--- src/EntryPointFactory.js
+++ src/EntryPointFactory.js
@@ -35,7 +35,7 @@ class EntryPointFactoryClass extends GObject.Object {
       return EntryPoint336;
     }
 
-    if (ShellVersion.startsWith('40')) {
+    if (ShellVersion.startsWith('40') || ShellVersion.startsWith('41')) {
       return EntryPoint40;
     }
 
